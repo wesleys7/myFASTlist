19ed4424aafe9643009086bdf09387ee28be7dc6
Wesley Souza <131703451+wesleys7@users.noreply.github.com>
2025-04-22 16:05:23 -0300

    watch created

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,30 @@
-alert ('hello word')
+
+function updateClock() {
+    let now = new Date
+    let h = now.getHours()
+    let m = now.getMinutes()
+
+    let daynames = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-Feira', 'Sábado']
+    let monthNames = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+    let day = daynames[now.getDay()]
+    let date = now.getDate()
+    let month = monthNames[now.getMonth()]
+    let year = now.getFullYear()
+
+
+
+    document.querySelector('.infoday').innerHTML = `${day}, dia ${date}`
+    document.querySelector('.hour').innerHTML = h<10? '0'+h:h
+    document.querySelector('.minutes').innerHTML = m<10? '0'+m:m
+    document.querySelector('.infomonth').innerHTML = `${month}, ${year}`
+
+}
+
+updateClock()
+setInterval(updateClock, 1000)
+
+setInterval(()=>{
+    let points = document.querySelector('.points')
+    points.style.visibility = (points.style.visibility ==='hidden' ? 'visible':'hidden')
+}, 1000)
