b22c27bed97eace4f7bf9445a7eed1964e56cd48
Wesley Souza <131703451+wesleys7@users.noreply.github.com>
2025-05-09 20:00:03 -0300

    menu option, color change in the developed background

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,75 @@
+//const
+
+const menuOptions = document.querySelector('.menuOptions')
+const menuListOptions = document.querySelector('.menuListOptions')
+const btnCloseMenuListOptions = document.querySelector('.btn-closeMenuListOptions')
+const overlay = document.querySelector('.overlayTransp')
+const theme = document.querySelector('.theme')
+const themecolors = document.querySelector('.themecolors')
+const body = document.querySelector('body')
+const defaultColor = document.querySelector('.defaultColor')
+const color1 = document.querySelector('.color1')
+const color2 = document.querySelector('.color2')
+const color3 = document.querySelector('.color3')
+const color4 = document.querySelector('.color4')
+const color5 = document.querySelector('.color5')
+const saveColor = document.querySelector('.saveColor')
+
+
+
+//EventListener
+
+menuOptions.addEventListener('click', () => {
+   menuListOptions.style.display = menuListOptions.style.display === 'flex' ? 'none':'flex'
+   overlay.style.display = 'flex'
+})
+btnCloseMenuListOptions.addEventListener('click', () => {
+    menuListOptions.style.display = menuListOptions.style.display === 'flex' ? 'none':'flex'
+    overlay.style.display = 'none'
+    themecolors.style.display = 'none'
+})
+
+theme.addEventListener('click', () => {
+    themecolors.style.display = themecolors.style.display === 'flex' ? 'none':'flex'
+})
+
+defaultColor.addEventListener('click', () => {
+    body.style.backgroundColor = '#7fb6ff'
+})
+color1.addEventListener('click', () => {
+    body.style.backgroundColor = '#204c85'
+})
+color2.addEventListener('click', () => {
+    body.style.backgroundColor = '#1d2e35'
+})
+color3.addEventListener('click', () => {
+    body.style.backgroundColor = '#838567'
+})
+color4.addEventListener('click', () => {
+    body.style.backgroundColor = '#b38bff'
+})
+color5.addEventListener('click', () => {
+    body.style.backgroundColor = '#f6b2ff'
+})
+
+saveColor.addEventListener('click', () => {
+    const bgSelected = getComputedStyle(body).backgroundColor
+    body.style.backgroundColor = bgSelected
+    localStorage.setItem('selectedColor', bgSelected)
+    themecolors.style.display = 'none'
+})
+
+//functions 
+
+function updateBackground () {
+    const savedColor = localStorage.getItem('selectedColor')
+    if(savedColor) {
+        body.style.backgroundColor = savedColor
+    }
+}
+
+updateBackground ()
+
 
 function updateClock() {
     let now = new Date
